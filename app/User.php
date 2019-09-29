ad883362bfc0e7148068810dc8de6e1717af8a47
Bilyk Andrii <yaapis@gmail.com>
2019-09-30 01:34:40 +0300

    test tlint on multiple files

--- app/User.php
+++ app/User.php
@@ -30,6 +30,7 @@ class User extends Authenticatable
     /**
      * The attributes that should be cast to native types.
      *
+     * @var array
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
